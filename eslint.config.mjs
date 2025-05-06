311287ce4d78a38cfea32ea4275e5ae3c87a1437
Lime <liumingxun@yeah.net>
2025-05-06 22:46:06 +0800

    feat(example): add astro content loader playground

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,8 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   pnpm: true,
+  astro: true,
+  ignores: [
+    '.astro/**/*',
+  ],
 })
--- utils.ts
+++ utils.ts
@@ -1,10 +1,9 @@
 import type { RichTextItemResponse } from '@notionhq/client/build/src/api-endpoints.d.ts'
-import { escapeHTML } from 'astro/runtime/server/escape.js'
 
 export function reduceRichText(richTextList: Array<RichTextItemResponse>) {
   return richTextList.reduce((mdText, item) => {
     if (item.type === 'text') {
-      let content = escapeHTML(item.plain_text)
+      let content = encodeURIComponent(item.plain_text)
       const href = item.href
       const color = item.annotations.color === 'default' ? '' : item.annotations.color
       const annotations = (Object.keys(item.annotations) as ('bold' | 'italic' | 'strikethrough' | 'underline' | 'code')[])
