63bfb63a59b3d77cb9fcd221ed852d53cb5d4f8e
Liumingxun <liumingxunnn@outlook.com>
2025-08-29 16:19:44 +0800

    refactor: adjust code structure (#28)
    * refactor: move `nzodify` packages
    
    * refactor: move `example` package
    
    * refactor: move core package
    
    - Update build config for new package structure

--- bunup.config.ts
+++ bunup.config.ts
@@ -1,14 +1,27 @@
-import { defineConfig } from 'bunup'
+import { defineWorkspace } from 'bunup'
+import { copy } from 'bunup/plugins'
 
-export default defineConfig([
+export default defineWorkspace([
   {
-    entry: './src/index.ts',
-    format: ['cjs', 'esm'],
-    splitting: true,
-    dts: true,
+    name: 'nzodify',
+    root: 'packages/nzodify',
+    config: {
+      entry: 'bin/index.ts',
+      plugins: [
+        copy('./dist/index.js', '../../dist/nzodify.js'),
+        copy('./dist/index.js', '../core/dist/nzodify.js'),
+      ],
+    },
   },
   {
-    entry: './scripts/zodify.ts',
-    splitting: true,
+    name: 'core',
+    root: 'packages/core',
+    config: {
+      entry: 'src/index.ts',
+      format: ['cjs', 'esm'],
+      plugins: [
+        copy('./dist/**/*', '../../dist'),
+      ],
+    },
   },
 ])
--- packages/nzodify/bin/index.ts
+++ packages/nzodify/bin/index.ts
@@ -2,10 +2,11 @@
 import { rm, writeFile } from 'node:fs/promises'
 import { tmpdir } from 'node:os'
 import { resolve } from 'node:path'
+import process from 'node:process'
 import { fileURLToPath } from 'node:url'
 import { Project } from 'ts-morph'
 import { generate } from 'ts-to-zod'
-import { collect } from './utils'
+import { collect } from '../utils'
 
 const originalPath = fileURLToPath(import.meta.resolve('@notionhq/client/build/src/api-endpoints.d.ts'))
 const extractTypesPath = resolve(tmpdir(), 'notion-extracted-property.d.ts')
@@ -40,16 +41,23 @@ extractSourceFile.saveSync()
 
 const notionZodFilePath = resolve(import.meta.dirname, './property.notion.zod.ts')
 
-await writeFile(
-  notionZodFilePath,
-  generate({
-    sourceText: extractSourceFile.getFullText(),
-  }).getZodSchemasFile('')
-    .replace(
-      'import { z } from "zod";',
-      'import { z } from "astro/zod"',
-    ),
-  'utf-8',
-)
+async function main() {
+  await writeFile(
+    notionZodFilePath,
+    generate({
+      sourceText: extractSourceFile.getFullText(),
+    }).getZodSchemasFile('')
+      .replace(
+        'import { z } from "zod";',
+        'import { z } from "astro/zod"',
+      ),
+    'utf-8',
+  )
 
-await rm(extractTypesPath)
+  await rm(extractTypesPath)
+}
+
+main().catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
