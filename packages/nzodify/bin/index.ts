97442bc13c949a1523f1fe9b001fd030b8c4838d
limx <liumingxun@yeah.net>
2025-09-09 14:00:05 +0800

    feat: add notion zod schema

--- packages/nzodify/bin/index.ts
+++ packages/nzodify/bin/index.ts
@@ -14,20 +14,36 @@ const sourceFile = project.addSourceFileAtPath(originalPath)
 sourceFile.addStatements(`
 type ValueOf<T> = T[keyof T]
 type PageProperties = PageObjectResponse['properties']
-export type PageProperty = ValueOf<PageProperties>`)
+export type PageProperty = ValueOf<PageProperties>
+
+export type PageBlock = ListBlockChildrenResponse['results']`)
 
 const propertyDecl = sourceFile.getTypeAliasOrThrow('PageProperty')
 const propertyType = propertyDecl.getType()
-
 propertyDecl.setType(propertyType.getText(propertyDecl)).addJsDoc(`@discriminator type`)
 
+const blockDecl = sourceFile.getTypeAliasOrThrow('BlockObjectResponse')
+blockDecl.addJsDoc(`@discriminator type`)
+const pageBlockDecl = sourceFile.getTypeAliasOrThrow('PageBlock')
+const pageBlockType = pageBlockDecl.getType()
+pageBlockDecl.setType(pageBlockType.getText(sourceFile))
+
 const propertyZodFilePath = resolve(import.meta.dirname, './property.notion.zod.ts')
+const blockZodFilePath = resolve(import.meta.dirname, './block.notion.zod.ts')
 
 if (import.meta.main) {
   const propertySourceText = extractWithDeps(project, ['PageProperty']).getFullText()
-
   writeFileSync(propertyZodFilePath, generate({
     sourceText: propertySourceText,
+  }).getZodSchemasFile('')
+    .replace(
+      'import { z } from "zod";',
+      'import { z } from "astro/zod"',
+    ))
+
+  const blockSourceText = extractWithDeps(project, ['PageBlock']).getFullText()
+  writeFileSync(blockZodFilePath, generate({
+    sourceText: blockSourceText,
   }).getZodSchemasFile('')
     .replace(
       'import { z } from "zod";',
